591fea06f57f2407cd28eb98fc809c5071fc2d6c
nathan cerisara <cerisara.nathan@gmail.com>
2018-01-26 14:22:33 +0100

    résolution de quelques bugs et amélioration du programme qui calcule des statistiques sur les instances téléchargées

--- scrap/calc_stats_ints.py
+++ scrap/calc_stats_ints.py
@@ -1,6 +1,6 @@
 #*-coding:utf-8-*
-import loadnathmasto,os,dateutil.parser
-#from prog import chem_dossier
+import loadnathmasto,os,dateutil.parser,operator,collections
+from collections import *
 chem_dossier="/home/nathan/mastocorpus/"
 
 def calc_stats(inst):
@@ -13,85 +13,94 @@ def calc_stats(inst):
     tab_acct=[]
     for i in loadnathmasto.json_parse(a):
         try:
-            toot_language.append( i['language'] )
-            toot_id.append( i['id'] )
             ac=i['account']['acct']
-            acc=ac.split("@")
-            accc=acc[0]
-            toot_account.append( accc )
-            dat=i['created_at']
-            dat=dateutil.parser.parse(dat)
-            toot_date.append( dat )
+            # si ac contient un @, alors le user n'appartient pas a cette instance: il ne faut donc pas le compter dans les stats de l'instance
+            # il ne faut comptabiliser QUE les toots des users sans @
+            if "@" not in ac:
+                #print(ac)
+                accc=ac
+                toot_account.append( accc )
+                toot_language.append( i['language'] )
+                dat=i['created_at']
+                dat=dateutil.parser.parse(dat)
+                toot_date.append( dat.year*100+dat.month )
+                toot_id.append( i['id'] )
         except:
-            #print("error : le programme n'arrive pas récuperer l'id et le language du toot, il est peut-etre abimé")
+            #print("error : le programme n'arrive pas récuperer la date et le language du toot, il est peut-etre abimé")
             error=True
-    languages=[]
-    nb_l=[]
-    for l in toot_language:
-         xx=0
-         jj=0
-         for ll in languages:
-            if ll == l:
-                nb_l[xx]+=1
-                jj+=1
-                break
-         if jj == 0:
-            languages.append( l )
-            nb_l.append( 1 )
-    lp=0
-    for n in nb_l:
-        if n > nb_l[lp]:
-            lp = n
-    nb_tot=0
-    for n in nb_l:
-        nb_tot+=n
-    nb_fr=0
-    for v in range(0,len(languages)-1):
-        if languages[v] == "fr":
-            nb_fr=nb_l[v]
-            break
-    account=[]
-    ndfoalc=[]
-    for h in list(set(toot_account)):
-        account.append(h)
-        ndfoalc.append(0)
-        for g in toot_account:
-            if h == g: ndfoalc[len(ndfoalc)-1]+=1
-    
-    cpa=0
-    for x in range(0,len(ndfoalc)-1):
-        if ndfoalc[x] > ndfoalc[cpa]:
-            cpa = x
+    assert len(toot_language)==len(toot_date)
+    assert len(toot_language)==len(toot_account)
+    assert len(toot_language)==len(toot_id)
 
-    ddate=[]
-    nb_d=[]
+    # Afficher le pourcentage des 5 langues les plus utilisees dans l'instance    
+    coLang = Counter(toot_language)
     
-    for t in list(set(toot_date)):
-        ddate.append(t)
-        nb_d.append(0)
-        for g in toot_date:
-            if t == g: nb_d[len(nb_d)-1]+=1
+    tot_lang=0
+    for t in coLang.values(): tot_lang+=t
+    
+    langs=coLang.most_common(5)
+    
+    langg = []
+    for n in langs:
+        langg.append( [n[0],n[1]] )
 
-    dqaelpdt=0
-    for x in range(0,len(nb_d)-1):
-        if nb_d[x] > nb_d[dqaelpdt]:
-            dqaelpdt = x
-        
+    for l in langg:
+        pc=int(l[1])/tot_lang*100
+        l.append(pc)        
+    lpu=0
+    
+    
+    
+    # TODO: afficher nb de toots dans l'instance pour les 6 derniers mois (en texte)
+    
+    
+    
+    coDate = Counter(toot_date)
+    dates = OrderedDict(sorted(coDate.items()))
+    
+    ddates=[]    
+    for a in dates.keys():
+        ddates.append([a,dates[a]])   
+    
+    if len(ddates) < 6:
+        ddates=ddates
+    else:
+        dddates=[]
+        for x in range(6):
+            dddates.append(ddates[x])
+        ddates=dddates
+    
+    
+    i="\nINSTANCE "+inst
+    m="\nles 6 mois les plus récents = "
+    for mm in ddates:
+        m+="le "+str(mm[0])+" il y a eu "+str(mm[1])+" toots  ,"
+    m=m[0:len(m)-1]
+    
+    l="\nles 5 langues les plus parlées = "
+    for ll in langg:
+        if ll[0] == None: ll[0] = "?"
+        l+="le "+ll[0]+" avec "+str(ll[1])+" utilisations soit "+str(ll[2])+"%    ,"
+    l=l[0:len(l)-1]
+    #print(i)
+    #print(m)
+    #print(l)
     
     try:
-        ra="\nLa langue la plus utilisée dans l'instance "+str(inst)+" est ''"+str(languages[lp])+"'' avec "+str(nb_l[lp])+" utilisation, il y a "+str(nb_fr/nb_tot*100)+"% de utilisation de la langue francaise et le compte qui a publié le plus de toots dans cette instance est "+str(account[cpa])+" il y a eu le plus de toot le "+str(ddate[dqaelpdt])
+        ra=i+m+l
         print(ra)
-        return ra 
+        if len(ddates) > 0 and ra != None: return ra 
     except:
         return ("\n erreur inconnue dans l'instance",inst)
         print("error")
 
 a=os.listdir(chem_dossier)
+#a=["instance.business"]
 t=open("statistiques_instances.txt","w")
 for b in a:
     try:
         p=calc_stats(b)
-        t.write("\n"+str(p))
+        if p != "None" : t.write("\n"+str(p))
     except:
         print("erreur avec le fichier",b)
         
